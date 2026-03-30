e886bdbe881aa39602517b24bd4853a73a20fdbe
Firasse123 <firasguizani2012@gmail.com>
2026-03-30 19:38:16 +0100

    update cvs controller

--- src/cvs/cvs.controller.ts
+++ src/cvs/cvs.controller.ts
@@ -12,8 +12,10 @@ import {
 import { CvsService } from './cvs.service';
 import { CvEntity } from './entities/cv.entity';
 import { CreateCvDto } from './dto/create-cv.dto';
+import { UpdateCvDto } from './dto/update-cv.dto';
 import { GenericController } from '../common/db/generic-crud.controller';
 import { StatParamDto } from './dto/stat-param-cv.dto';
+import { UpdateByCriteriaCvDto } from './dto/update-by-criteria-cv.dto';
 
 @Controller('cvs')
 export class CvsController extends GenericController<CvEntity> {
@@ -48,5 +50,6 @@ export class CvsController extends GenericController<CvEntity> {
   ) {
     return this.cvsService.createCv(dto);
   }
+
 
 }

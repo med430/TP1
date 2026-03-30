1fca1a1577bb99c494aa492299126c458f5f4a4a
Firasse123 <firasguizani2012@gmail.com>
2026-03-30 19:45:05 +0100

    feat(cvs-service): init CvsService with repository injection

--- src/cvs/cvs.service.ts
+++ src/cvs/cvs.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import {
+  FindOptionsWhere,
+  Repository,
+} from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { GenericCrud } from '../common/db/generic-crud.service';
+import { CvEntity } from './entities/cv.entity';
+import { UpdateCvDto } from './dto/update-cv.dto';
 
 @Injectable()
-export class CvsService {}
+export class CvsService extends GenericCrud<CvEntity> {
+  constructor(
+    @InjectRepository(CvEntity)
+    private readonly cvRepository: Repository<CvEntity>,
+  ) {
+    super(cvRepository);
+  }
+}

e74e51dfb79d3016b23f3c478d8939622055ace9
Firasse123 <firasguizani2012@gmail.com>
2026-03-30 19:34:45 +0100

    feat(cvs): create CvsModule skeleton

--- src/cvs/cvs.module.ts
+++ src/cvs/cvs.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CvsService } from './cvs.service';
 import { CvsController } from './cvs.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CvEntity } from './entities/cv.entity';
 
 @Module({
   controllers: [CvsController],
   providers: [CvsService],
+  imports: [TypeOrmModule.forFeature([CvEntity])],
 })
 export class CvsModule {}

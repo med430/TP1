3b0c437c19b8858d904bd7022bad9ac485993823
Firasse123 <firasguizani2012@gmail.com>
2026-03-30 17:48:43 +0100

    Added the Get Routes

--- src/cvs/cvs.controller.ts
+++ src/cvs/cvs.controller.ts
@@ -11,11 +11,8 @@ import {
 } from '@nestjs/common';
 import { CvsService } from './cvs.service';
 import { CvEntity } from './entities/cv.entity';
-import { CreateCvDto } from './dto/create-cv.dto';
-import { UpdateCvDto } from './dto/update-cv.dto';
 import { GenericController } from '../common/db/generic-crud.controller';
 import { StatParamDto } from './dto/stat-param-cv.dto';
-import { UpdateByCriteriaCvDto } from './dto/update-by-criteria-cv.dto';
 
 @Controller('cvs')
 export class CvsController extends GenericController<CvEntity> {
@@ -44,50 +41,5 @@ export class CvsController extends GenericController<CvEntity> {
     return this.cvsService.findOne(id);
   }
 
-  @Post()
-  create(
-    @Body() dto: CreateCvDto,
-  ) {
-    return this.cvsService.createCv(dto);
-  }
-
-
-  @Patch(':id')
-  async update(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() dto: UpdateCvDto,
-
-  ): Promise<CvEntity> {
-      return this.cvsService.updateCv(id, dto);
-  }
-
-
-  @Delete(':id')
-  async delete(
-    @Param('id', ParseIntPipe) id: number,
-  ) {
-   return this.cvsService.softDelete(id);
-
-  }
-
-
-  @Patch()
-  updateByCriteria(
-    @Body() body: UpdateByCriteriaCvDto,
-  ) {
-    return this.cvsService.updateByCriteriaCv(
-      body.criteria,
-      body.dto
-    );
-  }
-
-  @Patch(':id/restore')
-  restore(@Param('id', ParseIntPipe) id: number) {
-    return this.cvsService.restore(id);
-  }
-
-  @Delete(':id/hard')
-  hardDelete(@Param('id', ParseIntPipe) id: number) {
-    return this.cvsService.delete(id);
-  }
+  
 }

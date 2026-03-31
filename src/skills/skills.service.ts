60c85c17a0cd3349d07eb1b8eb1894e0ee2a6673
azizkchaou <165052608+azizkchaou@users.noreply.github.com>
2026-03-31 18:56:36 +0100

    connect skills service

--- src/skills/skills.service.ts
+++ src/skills/skills.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+} from '@nestjs/common';
+import {
+  Repository,
+} from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { GenericCrud } from '../common/db/generic-crud.service';
+import { SkillEntity } from './entities/skill.entity';
 
 @Injectable()
-export class SkillsService {}
+export class SkillsService extends GenericCrud<SkillEntity> {
+  constructor(
+    @InjectRepository(SkillEntity)
+    private readonly skillRepository: Repository<SkillEntity>,
+  ) {
+    super(skillRepository);
+  }
+
+
+}

86721b4cd44cb3d95561a745445b4986f8d15192
azizkchaou <165052608+azizkchaou@users.noreply.github.com>
2026-03-31 18:53:54 +0100

    register skill entity in skills module

--- src/skills/skills.module.ts
+++ src/skills/skills.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SkillsService } from './skills.service';
 import { SkillsController } from './skills.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SkillEntity } from './entities/skill.entity';
 
 @Module({
   controllers: [SkillsController],
   providers: [SkillsService],
+  imports: [TypeOrmModule.forFeature([SkillEntity])],
 })
 export class SkillsModule {}

2a91974fe6428712dfc6bbd966555731395b9f74
Mekki Wissem <wissem.mekki@insat.ucar.tn>
2026-04-03 17:49:32 +0100

    register auth middleware in CvsModule

--- src/cvs/cvs.module.ts
+++ src/cvs/cvs.module.ts
@@ -1,12 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CvsService } from './cvs.service';
 import { CvsController } from './cvs.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CvEntity } from './entities/cv.entity';
+import { AuthMiddleware } from '../auth/auth.middleware';
 
 @Module({
   controllers: [CvsController],
   providers: [CvsService],
   imports: [TypeOrmModule.forFeature([CvEntity])],
 })
-export class CvsModule {}
+export class CvsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes('cvs');
+  }
+}
